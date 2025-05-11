277a8eeb48400232fda48eb043e1cf6184b53c94
kyaaddd <syakillaputrim@gmail.com>
2025-05-11 21:48:47 +0700

    Add files via upload

---
+++ resources/views/admin/partials/footer.blade.php
@@ -0,0 +1,3 @@
+<div class="py-6 px-6 text-center">
+    <p class="mb-0 fs-4">Dashboard Admin Desa Cikasungka Copyright 2025</p> Repost By : <a href="https://ypt.or.id/">Yayasan Pendidikan Telkom</a>
+  </div>
